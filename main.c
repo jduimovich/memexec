83c5b752b5b78267e11725763ad1cd91c089c521
John Duimovich <jduimovich@gmail.com>
2021-05-09 09:52:44 -0400

    include malloc mem test

--- main.c
+++ main.c
@@ -8,6 +8,13 @@ int main(int argc, char *argv[]) {
   executable_code [0] = 0xC3; // flat mode near return 
   function_call *f = (function_call *)&executable_code[0];
   (*f) ();
+
+
+  char * allocated = (char*) malloc (128);
+  allocated [0] = 0xC3; // flat mode near return 
+  f = (function_call *)&allocated[0];
+  (*f) ();
+
   printf("Done Exec Memory Tests\n"); 
   return 0; 
 }
