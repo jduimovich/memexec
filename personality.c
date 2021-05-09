41622f22818bab2cb90768f02184b0f60425690f
John Duimovich <jduimovich@gmail.com>
2021-05-09 09:21:50 -0400

    personality example

---
+++ personality.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#incude "personality.h"
+
+char executable_code[128]; 
+typedef void(function_call)();
+
+int main(int argc, char *argv[]) { 
+  printf("Exec Memory Tests Using personality\n"); 
+  long pers = personality(0xffffffffUL);
+  printf("Personality == %lx\n", pers); 
+  long new_pers = personality(pers | ADDR_NO_RANDOMIZE | READ_IMPLIES_EXEC);
+  
+
+  long pers = personality(0xffffffffUL);
+  printf("Personality now == %lx\n", pers);    
+
+  executable_code [0] = 0xC3; // flat mode near return 
+  function_call *f = (function_call *)&executable_code[0];
+  (*f) ();
+  printf("Done Exec Memory Tests\n"); 
+  return 0; 
+}
