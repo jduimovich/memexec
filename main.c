1aa42346b6df4e891f89fa39f5d2751580005135
John Duimovich <jduimovich@gmail.com>
2021-05-09 10:38:46 -0400

    split out var name

--- main.c
+++ main.c
@@ -13,20 +13,20 @@ the code wiil abort with Trace/breakpoint trap
 int main(int argc, char *argv[]) { 
   printf("Exec code in static memory\n"); 
   executable_code [0] = 0xC3; // flat mode near return 
-  function_call *f = (function_call *)&executable_code[0];
-  (*f) (); 
+  function_call *f_static = (function_call *)&executable_code[0];
+  (*f_static) (); 
  
   printf("Exec code in malloc memory\n"); 
   char * allocated = (char*) malloc (128);
   allocated [0] = 0xC3; // flat mode near return 
-  f = (function_call *)&allocated[0];
-  (*f) ();   
+  function_call *f_malloc = (function_call *)&allocated[0];
+  (*f_malloc) ();   
 
   printf("Exec code in stack memory\n"); 
   char * stack[128];
   stack [0] = 0xC3; // flat mode near return 
-  function_call *f = (function_call *)&stack[0];
-  (*f) (); 
+  function_call *f_stack = (function_call *)&stack[0];
+  (*f_stack) (); 
 
   return 0; 
 }
