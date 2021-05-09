ffe1e6c109109de79ffc8fd4d3fc7517257d300f
John Duimovich <jduimovich@gmail.com>
2021-05-09 09:09:26 -0400

    more printing

--- demo.sh
+++ demo.sh
@@ -5,7 +5,23 @@ cc main.c -o  demo_no_exec
 echo Running demo_w_exec - compiled with -z execstack 
 echo Expect this to SUCCEED 
 ./demo_w_exec
+ERR=$?
+if [ $ERR -eq 0 ]
+then
+  echo "Correct - Successfully ran executable code"
+else
+  echo "Fail to run executable code" 
+fi
+
+echo -n 
 
 echo Running demo_w_exec - compiled without -z execstack 
 echo Expect this to CRASH 
 ./demo_no_exec
+ERR=$?
+if [ $ERR -eq 0 ]
+then
+  echo "Correct - No Exec Stack should crash"
+else
+  echo "FAIL - No Exec Stack should not succeed" 
+fi
