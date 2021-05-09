ef111735075f38b9ac18bacceacd91a359cfdc96
John Duimovich <jduimovich@gmail.com>
2021-05-09 10:01:40 -0400

    new modes

--- main.c
+++ main.c
@@ -5,17 +5,17 @@ char executable_code[128];
 typedef void(function_call)();
 
 int main(int argc, char *argv[]) { 
-  printf("Exec Memory Tests\n"); 
+  printf("Exec in static memory\n"); 
   executable_code [0] = 0xC3; // flat mode near return 
   function_call *f = (function_call *)&executable_code[0];
   (*f) ();
+  printf("Done Exec in static memory\n"); 
 
-
+  printf("Exec in malloc memory\n");     
   char * allocated = (char*) malloc (128);
   allocated [0] = 0xC3; // flat mode near return 
   f = (function_call *)&allocated[0];
-  (*f) ();
-
-  printf("Done Exec Memory Tests\n"); 
+  (*f) ();  
+  printf("Done Exec in malloc memory\n");  
   return 0; 
 }
--- personality.c
+++ personality.c
@@ -25,9 +25,19 @@ void change_personality () {
 int main(int argc, char *argv[]) { 
   printf("Exec Memory Tests Using personality\n"); 
   change_personality ();
+
+  printf("Exec in static memory\n"); 
   executable_code [0] = 0xC3; // flat mode near return 
   function_call *f = (function_call *)&executable_code[0];
   (*f) ();
-  printf("Done Exec Memory Tests\n"); 
+  printf("Done Exec in static memory\n"); 
+
+  printf("Exec in malloc memory\n");     
+  char * allocated = (char*) malloc (128);
+  allocated [0] = 0xC3; // flat mode near return 
+  f = (function_call *)&allocated[0];
+  (*f) ();  
+  printf("Done Exec in malloc memory\n");  
+  
   return 0; 
 }
