58f30d73246c275e6a92cf4517f7e076a7c62942
John Duimovich <jduimovich@gmail.com>
2021-05-09 09:40:51 -0400

    simple demo scrips

--- demo.sh
+++ demo.sh
@@ -2,8 +2,11 @@
 cc -z execstack main.c -o  demo_w_exec
 cc main.c -o  demo_no_exec
 
+cc personality.c -z execstack -o  demo_personality_w_exec
+cc personality.c -o  demo_personality_no_exec
+
 echo Running demo_w_exec - compiled with -z execstack 
-echo Expect this to SUCCEED 
+#Expect this to SUCCEED 
 ./demo_w_exec
 ERR=$?
 if [ $ERR -eq 0 ]
@@ -16,12 +19,33 @@ fi
 echo -n 
 
 echo Running demo_w_exec - compiled without -z execstack 
-echo Expect this to CRASH 
+#Expect this to CRASH 
 ./demo_no_exec
 ERR=$?
 if [ $ERR -eq 0 ]
 then
-  echo "FAIL - Without -z execstack this should crash" 
+  echo "FAIL - Without -z execstack this should crash but did not" 
 else
   echo "SUCCESS - Without -z execstack this should crash"  
 fi
+
+
+echo Running demo_personality_w_exec - compiled with -z execstack 
+./demo_personality_w_exec
+ERR=$?
+if [ $ERR -eq 0 ]
+then 
+  echo "SUCCESS - with -z execstack should succeed"  
+else
+  echo "FAILED - with -z execstack should succeed"  
+fi
+
+echo Running demo_personality_w_exec - compiled with -z execstack 
+./demo_personality_no_exec
+ERR=$?
+if [ $ERR -eq 0 ]
+then 
+  echo "SUCCESS - without -z execstack this should now succeed"  
+else
+  echo "FAILED - without -z execstack this should now succeed but did not"   
+fi 
