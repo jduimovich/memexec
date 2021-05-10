3c99eac3cddb60b5790fe6ee50e2f71c2e9cf4c3
John Duimovich <jduimovich@gmail.com>
2021-05-10 07:56:36 -0400

    clean up memory range

--- malloctest.c
+++ malloctest.c
@@ -26,8 +26,9 @@ you can use a 0xCC which is a breakpoint
 the code wiil abort with Trace/breakpoint trap
 */
 
-int main(int argc, char *argv[]) { 
+#define ALLOCATE_SIZE 64
 
+int main(int argc, char *argv[]) {  
   int pagesize = sysconf(_SC_PAGE_SIZE);
   printf("_SC_PAGE_SIZE %d\n", pagesize); 
   if (sizeof (void*) != 8) {  
@@ -40,25 +41,26 @@ int main(int argc, char *argv[]) {
     /* aligns the memory returned via mprotect skips front of allocated to align 
        over allocate the memory to allow for alignment (need an extra pagesize-1 bytes)
     */
-    char * raw = (char*) malloc (pagesize*2);
+    char * raw = (char*) malloc (ALLOCATE_SIZE+(pagesize-1));
     char * allocated =  align_to(raw, pagesize); 
     printf ("Trying Allocated %p\n",allocated);  
-    make_rwx(allocated, pagesize) ;
+    make_rwx(allocated, ALLOCATE_SIZE-(allocated - raw)) ;
     allocated [0] = 0xC3; // flat mode near return 
     function_call *f_malloc = (function_call *)&allocated[0];
     (*f_malloc) ();   
   }
 
   { 
-    printf("Exec code in malloc memory\n"); 
+    printf("Exec code in malloc memory, mprotect surrounding pages\n"); 
     /* Hack version, align the pages around the allocated pointer
-       Do not change the pointer, just adjust for alignment 
+       Do not change the pointer, just force adjust for mprotect
+       USE the ORIGINAL pointer for execution 
     */
-    char * allocated = (char*) malloc (pagesize*2);
+    char * allocated = (char*) malloc (ALLOCATE_SIZE);
     long aligned = (((long)allocated) & ~(pagesize-1));  
     char * protect =  (char*) aligned;  
-    // add the rounded down to length
-    make_rwx(protect, (allocated - protect)+pagesize*2) ;
+    // add the rounded down to allocate size
+    make_rwx(protect, (allocated - protect)+ALLOCATE_SIZE) ;
     allocated [0] = 0xC3; // flat mode near return 
     function_call *f_malloc = (function_call *)&allocated[0];
     (*f_malloc) ();   
@@ -66,11 +68,10 @@ int main(int argc, char *argv[]) {
 
   {
     printf("Exec code in memalign memory\n"); 
-    /* memalign and mprotect  
-    */ 
-    char * allocated =  (char*) aligned_alloc(pagesize, pagesize); 
-    printf ("memalign Allocated %p\n",allocated);  
-    make_rwx(allocated, pagesize) ;
+    /* aligned_alloc and mprotect  */ 
+    char * allocated =  (char*) aligned_alloc(pagesize, ALLOCATE_SIZE); 
+    printf ("aligned_alloc Allocated %p\n",allocated);  
+    make_rwx(allocated, ALLOCATE_SIZE) ;
     allocated [0] = 0xC3; // flat mode near return 
     function_call *f_malloc = (function_call *)&allocated[0];
     (*f_malloc) ();   
